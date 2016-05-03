42953d5fdba58021e1dc85bbdea0251a7172f5b0
abalone0204 <abalone0204@gmail.com>
2016-05-04 02:31:19 +0800

    done insertion sort

--- chapter-1/insertion_sort.js
+++ chapter-1/insertion_sort.js
@@ -1,14 +1,30 @@
+"use strict";
+// Pseudo code:
+// for j=2 to A[length]
+//   key = A[j]
+//   // Insert A[j] into the sorted sequence A[1 ..j-1]
+//   i = j-1
+//   while (i >0 and A[i] >key)
+//      A[i+1] = A[i]
+//      i = i -1
+//   A[i+1] = key
 function insertionSort(list) {
     if (list.length === 1) {
         return list
     }
-    const sorted = [] 
-    const length = list.length
-    list.forEach((val, key) => {
-        const n = val
-        let idx = 
-        while()
-    })    
+    const sorted = list.slice()
+    for(let j=1 ; j < list.length - 1; j++) {
+        // 暫存當前要比較的值
+        let key = sorted[j]
+        let idx = j - 1
+        while(idx>=0 && sorted[idx] > key) {
+            // 將前一個值跟現在要比較的值所處的位置對調
+            sorted[idx+1] = sorted[idx]
+            idx -= 1
+        }
+        sorted[idx+1] = key
+    }
+    return sorted
 }
 
 
