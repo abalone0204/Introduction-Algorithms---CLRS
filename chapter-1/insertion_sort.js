81aced87dd1633f94a8322270b6f0629b82d173c
abalone0204 <abalone0204@gmail.com>
2016-05-04 01:21:32 +0800

    Init

---
+++ chapter-1/insertion_sort.js
@@ -0,0 +1,16 @@
+function insertionSort(list) {
+    if (list.length === 1) {
+        return list
+    }
+    const sorted = [] 
+    const length = list.length
+    list.forEach((val, key) => {
+        const n = val
+        let idx = 
+        while()
+    })    
+}
+
+
+const result = insertionSort([2,4,1,3,7,9,13])
+console.log(result);
